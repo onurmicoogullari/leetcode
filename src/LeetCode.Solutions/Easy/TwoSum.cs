a2df7866464b208be1c898e8cd138896a1f5542a
Onur Micoogullari <204898@skane.se>
2024-04-05 23:30:40 +0200

    Fixed formatting.

--- src/LeetCode.Solutions/Easy/TwoSum.cs
+++ src/LeetCode.Solutions/Easy/TwoSum.cs
@@ -8,12 +8,8 @@ public static class TwoSum
         Link: https://leetcode.com/problems/two-sum/description/
         
         Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
         You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
         You can return the answer in any order.
-
- 
 
         Example 1:
 
